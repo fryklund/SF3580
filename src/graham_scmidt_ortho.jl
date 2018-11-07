05d7f113ee91d79fd2e25bd95b284e756441921c
Fredrik Fryklund <fryklund.fredrik@gmail.com>
2018-11-07 11:59:59 +0100

    Improved graham_schmidt_ortho.jl by removing one for-loop

---
+++ src/graham_scmidt_ortho.jl
@@ -0,0 +1,21 @@
+function graham_schmidt_ortho(A::Matrix)
+# Q*R = A
+# Q' *  Q = Q * Q' = I
+    (n_row,n_col) = size(A);
+    # Check if A has the correct structure
+    if n_col > n_row
+        println("ERROR: number of columns > number of rows")
+        println("return")
+        return nothing,nothing
+    end
+    # Graham Scmidt orthogonalization
+    Q = copy(A);
+    R = zeros(n_col,n_col)
+    for i = 1:n_col
+        R[1:i-1,i] = Q[:,1:i-1]' * Q[:,i];
+        Q[:,i] = Q[:,i] - Q[:,1:i-1] * R[1:i-1,i];
+        R[i,i] = norm(Q[:,i]);
+        Q[:,i] = Q[:,i] / R[i,i];
+    end
+    return Q,R
+end
