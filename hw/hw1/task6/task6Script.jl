cd1a978296ccf0e684f270baf3a3bb0c3391ea7e
Fredrik Fryklund <fryklund.fredrik@gmail.com>
2018-11-07 15:41:08 +0100

    Changed back to previous GS

--- hw/hw1/task6/task6Script.jl
+++ hw/hw1/task6/task6Script.jl
@@ -1,2 +1,18 @@
+using MAT
+using LinearAlgebra
+using PyCall
+using PyPlot
+
 include("../../../src/graham_scmidt_ortho.jl")
 B=matread("Bwedge.mat")["B"];
+B_eigvals = eigvals(B);
+
+println("Plotting")
+figure()
+plot(real(B_eigvals),imag(B_eigvals),"bo")
+plot(real(B_eigvals[1:3]),imag(B_eigvals[1:3]),"ro")
+xlabel("Re")
+ylabel("Im")
+title("Eigenvalues of B")
+
+savefig("task6a.png")
--- src/graham_scmidt_ortho.jl
+++ src/graham_scmidt_ortho.jl
@@ -9,14 +9,18 @@ function graham_schmidt_ortho(A::Matrix)
         return nothing,nothing
     end
     # Graham Scmidt orthogonalization
-    Q = copy(A);
+    Q = zeros(n_row,n_col)
     R = zeros(n_col,n_col)
+    R[1,1] = norm(A[:,1])
+    #v = zeros(n_row)
     for i = 1:n_col
-        R[1:i-1,i] = Q[:,1:i-1]' * Q[:,i];
-        Q[:,i] = Q[:,i] - Q[:,1:i-1] * R[1:i-1,i];
-        R[i,i] = norm(Q[:,i]);
-        Q[:,i] = Q[:,i] / R[i,i];
-
+        v = A[:,i];
+        for j=1:(i-1)
+            R[j,i] = Q[:,j]' * A[:,i];
+            v = v - R[j,i] * Q[:,j];
+        end
+        R[i,i] = norm(v);
+        Q[:,i] = v / R[i,i];
     end
     return Q,R
 end
