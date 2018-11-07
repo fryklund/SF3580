d0194f4e7c09cd0cd6d2f700a2d7c9f58de96b91
Fredrik Fryklund <fryklund.fredrik@gmail.com>
2018-11-07 12:58:41 +0100

    Resolved conflicts

--- hw/hw1/task6/task6Script.jl
+++ hw/hw1/task6/task6Script.jl
@@ -1,5 +1,2 @@
 include("../../../src/graham_scmidt_ortho.jl")
 B=matread("Bwedge.mat")["B"];
-A = [1 2;3 4;4 5];
-Q,R = graham_schmidt_ortho(A)
-Q*Q'
--- src/graham_scmidt_ortho.jl
+++ src/graham_scmidt_ortho.jl
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
-function graham_schmidt_ortho(A)
-# R = Q*A
-# Q' *  Q = Q * Q' = I
-# QR-decomposition: A = Q'*R
-=======
 function graham_schmidt_ortho(A::Matrix)
 # Q*R = A
 # Q' *  Q = Q * Q' = I
->>>>>>> dev
     (n_row,n_col) = size(A);
     # Check if A has the correct structure
     if n_col > n_row
@@ -16,20 +9,6 @@ function graham_schmidt_ortho(A::Matrix)
         return nothing,nothing
     end
     # Graham Scmidt orthogonalization
-<<<<<<< HEAD
-    Q = zeros(n_row,n_col)
-    R = zeros(n_col,n_col)
-    R[1,1] = norm(A[:,1])
-    #v = zeros(n_row)
-    for i = 1:n_col
-        v = A[:,i];
-        for j=1:(i-1)
-            R[j,i] = Q[:,j]' * A[:,i];
-            v = v - R[j,i] * Q[:,j];
-        end
-        R[i,i] = norm(v);
-        Q[:,i] = v / R[i,i];
-=======
     Q = copy(A);
     R = zeros(n_col,n_col)
     for i = 1:n_col
@@ -37,7 +16,7 @@ function graham_schmidt_ortho(A::Matrix)
         Q[:,i] = Q[:,i] - Q[:,1:i-1] * R[1:i-1,i];
         R[i,i] = norm(Q[:,i]);
         Q[:,i] = Q[:,i] / R[i,i];
->>>>>>> dev
+
     end
     return Q,R
 end
