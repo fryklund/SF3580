c29523ce99eb69c06a418f214496c722ff2f4dc9
annabroms <annabrom@kth.se>
2018-11-29 21:27:20 +0100

    complete task 5

--- hw/hw2/task6/task6.jl
+++ hw/hw2/task6/task6.jl
@@ -9,48 +9,65 @@ b=matread("Bwedge2.mat")["b"];
 
 F = eigen(Matrix(B))
 eig = F.values
-
+vcond = cond(F.vectors)
+println(vcond)
 
 x_ex = B\b;
 bnorm = norm(b);
+global c1 = 36.5
+global r1 = 20
+global c2 = eig[1]
+println(sqrt(r1*(r1+abs(c1-c2))/abs(c1*c2)))
+
+global c = 30;
+global r = 28
+println(r/abs(c))
 
 
 
-for k = 1:20
+
+for k = 1:100
     println(k)
     global itr = k
-    t_gmres = @belapsed gmres(B,b[:,1],itr)
+    #t_gmres = @belapsed gmres(B,b[:,1],itr)
     x,r_log = gmres(B,b[:,1],k)
-    t_cg = @belapsed cgne(B,b[:,1],itr)
+    #t_cg = @belapsed cgne(B,b[:,1],itr)
     x_cg = cgne(B,b[:,1],k)
+    co = cond(Matrix(B'*B))
 
-    #figure(1)
-    #semilogy(k,r_log[k]*bnorm,"r.")
-    #semilogy(k,norm(x_ex[:,1].-x_cg),"b.")
-    figure(2)
-    semilogy(t_gmres,r_log[k]*bnorm,"r.")
-    semilogy(t_cg,norm(x_ex[:,1].-x_cg),"b.")
+    figure(1)
+    semilogy(k,r_log[k]*bnorm,"r.")
+    semilogy(k,norm(x_ex[:,1].-x_cg),"b.")
+    #semilogy(k,vcond*(sqrt(r1*(r1+abs(c1-c2))/abs(c1*c2)))^(k-1),"k.")
+    semilogy(k,vcond*(r/abs(c))^(k-1),"k.")
+    semilogy(k,2*((sqrt(co)-1)/(sqrt(co)+1))^k,"g.")
+    #figure(2)
+    #semilogy(t_gmres,r_log[k]*bnorm,"r.")
+    #semilogy(t_cg,norm(x_ex[:,1].-x_cg),"b.")
+
 
 end
 
 figure(1)
 xlabel("Iteration")
 ylabel(L"$\|Ax-b\|_2$")
-legend(["gmres","cgne"])
+legend(["gmres","cgne","gmres convergence bound","cgne convergence bound"])
 savefig("error_itr.png")
 close(1)
 
-figure(2)
-xlabel("CPU-time (seconds)")
-ylabel(L"$\|Ax-b\|_2$")
-legend(["gmres","cgne"])
-savefig("error_time.png")
-close(2)
+#figure(2)
+#xlabel("CPU-time (seconds)")
+#ylabel(L"$\|Ax-b\|_2$")
+#legend(["gmres","cgne"])
+#savefig("error_time.png")
+#close(2)
 
 
 
 figure(3)
 plot(real(eig),imag(eig),".")
+t = 0:0.01:2*pi
+plot(c.+r*cos.(t),r*sin.(t))
 xlabel("Real part of eigenvalues")
 ylabel("Imaginary part of eigenvalues")
 savefig("Eigens.png")
