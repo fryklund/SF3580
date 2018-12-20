cafa694908964fdc2c959c1f35fcec45a6ad73e6
Anna Broms <annabrom@kth.se>
2018-12-20 16:12:52 +0100

    complete task 1

---
+++ hw/hw4/task1/lyap0_mult.m
@@ -0,0 +1,5 @@
+function [] = lyap0_mult(A,P)
+    for k = 1:10
+        X = lyap0(A,P);
+    end
+end
---
+++ hw/hw4/task1/naiveLyap_mult.m
@@ -0,0 +1,6 @@
+function [] = naiveLyap_mult(A,P)
+for k = 1:10
+    X = naiveLyap(A,P);
+end
+    
+end
